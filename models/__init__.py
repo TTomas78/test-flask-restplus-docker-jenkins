683a7de8ebdc45cf2a9f1e2719f5553c7c7bd530
Tomas Bourgeois <bourgeoistomas@gmail.com>
2019-07-25 16:10:23 -0300

    commit

--- models/__init__.py
+++ models/__init__.py
@@ -3,3 +3,4 @@ from models.test2_model import ConditionModel
 from models.test_3_model import test432Model
 from models.test5_model import TestModel
 from models.test45_model import TomasModel
+from models.test534_model import testingmd
--- models/test534_model.py
+++ models/test534_model.py
@@ -1,12 +1,10 @@
 from app import db
 
-class ClientModel(db.Model):
-    __tablename__ = 'client_accounts'
+class testingmd(db.Model):
+    __tablename__ = 'modelss'
     __table_args__ = {'schema': 'public'}
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    address1 = db.Column(db.String(100))
     address3 = db.Column(db.String(100))
-    zip_code = db.Column(db.String(50))
     city = db.Column(db.String(100))
     # state = db.Column(db.String(100))
