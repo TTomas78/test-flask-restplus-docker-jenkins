0dbe3777f1a77fd910f646c90eacc3e44a4eaec6
Juan Rabadan <juan@arzion.com>
2019-07-25 14:09:08 -0300

    jony merged

---
+++ migrations/versions/9695c82639d7_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 9695c82639d7
+Revises: a2a47e51eb54, c1de32a4899d
+Create Date: 2019-07-25 13:50:14.948931
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9695c82639d7'
+down_revision = ('a2a47e51eb54', 'c1de32a4899d')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
